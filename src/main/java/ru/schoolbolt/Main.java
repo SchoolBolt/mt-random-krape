1574f349e2eec0deb60806018dab2d303828fdae
agentprog <39810295+agentprog@users.noreply.github.com>
2020-03-19 17:09:06 +0500

    Initial commit

---
+++ src/main/java/ru/schoolbolt/Main.java
@@ -0,0 +1,13 @@
+package ru.schoolbolt;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    static final Scanner scanner = new Scanner(System.in);
+    static final Random random = new Random();
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
